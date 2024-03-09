68faa708f4c50ddb92dbe3a0a0c528f67706a295
Danila <71084051+Danila-cyber@users.noreply.github.com>
2024-03-09 22:53:48 +0300

    Update main.py
    1.2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#ННатуральные числа, в которых строго чередуются четные и нечетные цифры. Список используемых в числах четных цифр выводить отдельно прописью. - Вариант 20
+#Натуральные числа, в которых строго чередуются четные и нечетные цифры. Список используемых в числах четных цифр выводить отдельно прописью. - Вариант 20
 
 def checkNum(strNum):
-  if strNum.isdigit():
+  if "-" in strNum or strNum == "": return False
+  else:
     flag = True
     oneChar = True if int(strNum[0])%2==0 else False
     for i in range(1, len(strNum)):
@@ -10,10 +11,10 @@ def checkNum(strNum):
         oneChar = char
       else: flag = False
     return flag
-  else: return False
 
 nums = []
 fileName = "ref1.txt"
+dc_cifr = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
 try:
   open(fileName, "r")
 except:
@@ -23,15 +24,22 @@ with open(fileName, "r") as file:
   while True:
     line = file.readline().replace('\n', '')
     if not(line): break
-    if checkNum(line): nums.append(line)
-      
+    if (line.replace(" ", "")).isalpha() == False and (line.replace(" ", "")).isdigit() == False: continue
+    num = ""
+    if line.isdigit(): nums.append(line)
+    else:
+      for index in range(0, len(line)):
+        if line[index].isdigit():
+          num += line[index]
+        if not(line[index].isdigit()) or index == (len(line) - 1):
+          if checkNum(num): nums.append(num)
+          num = ""
           
 for num in nums:
   print(num)
   acceptChar = []
   for char in num:
     if int(char) % 2 == 0: acceptChar.append(char)
-  print("Четные цифры этого числа: ")
-  print(", ".join(acceptChar))
-  print("|____________________|")
+  print("Четные цифры этого числа: " + ", ".join(list(map(lambda x: dc_cifr[x], acceptChar))))
+  print("____________________")
 
